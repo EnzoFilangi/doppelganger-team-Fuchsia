b400d6303abed5178d31efc125aed30b9528847a
EnzoFilangi <enzo.filangi@efrei.net>
2022-11-09 09:52:35 +0100

    Added test:jest command to package.json to be able to test from the CLI with jest

---
+++ js/tests/calculator.test.js
@@ -0,0 +1,5 @@
+test('should throw when not authorized', () => {
+  // TODO: write a test that fails due to the bug in
+  // Calculator.divide()
+
+})
--- js/tests/calculotor.test.js
+++
@@ -1,7 +0,0 @@
-const test = require('tape')
-
-test('should throw when not authorized', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // Calculator.divide()
-  t.end()
-})
