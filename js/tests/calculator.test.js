3b1defc5f2aa18fce022a33b1aaba23e02022278
EnzoFilangi <enzo.filangi@efrei.net>
2022-11-09 10:17:47 +0100

    Failing test for calculator.js

--- js/tests/calculator.test.js
+++ js/tests/calculator.test.js
@@ -1,5 +1,14 @@
+const Calculator = require('../calculator')
+
 test('should throw when not authorized', () => {
   // TODO: write a test that fails due to the bug in
   // Calculator.divide()
 
+  const mockAuthorizer = {
+    authorize: jest.fn(() => false)
+  };
+
+  const calculator = new Calculator(mockAuthorizer);
+
+  expect(calculator.divide(1, 2)).toThrow();
 })
