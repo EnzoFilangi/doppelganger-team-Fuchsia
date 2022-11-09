d9713733a132693f3be3f8e72e3caa3a30837f0e
Sami-EB <dutinfo123@gmail.com>
2022-11-09 09:12:22 +0100

    discount applier 1

--- js/tests/discount-applier.test.js
+++ js/tests/discount-applier.test.js
@@ -1,4 +1,13 @@
 const test = require('tape')
+const DiscountApplier = require('../discount-applier');
+
+class Notifier {
+  constructor () {
+  }
+  notify (user,message) {
+    console.log(user + ' ' + message);
+  }
+}
 
 test('apply v1', (t) => {
   // TODO: write a test that fails due to the bug in
@@ -7,7 +16,15 @@ test('apply v1', (t) => {
 })
 
 test('apply v2', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // DiscountApplier.applyV2
+
+  let notifier = new Notifier();
+
+  let applier = new DiscountApplier(notifier);
+
+  let discount = 10;
+  let users = ['user1','user2','user3','user4'];
+
+  applier.applyV2(discount,users);
+
   t.end()
 })
