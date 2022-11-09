83b7d417a697735db820a8a8c7a9521c3318cd04
EnzoFilangi <enzo.filangi@efrei.net>
2022-11-09 09:53:57 +0100

    Formatted test files to use jest instead of tape

--- js/tests/mail-sender.test.js
+++ js/tests/mail-sender.test.js
@@ -1,14 +1,11 @@
 const sender = require('../mail-sender')
-const test = require('tape')
 
-test('send v1', (t) => {
+test('send v1', () => {
   // TODO: write a test that fails due to the bug in
   // MailSender.sendV1
-  t.end()
 })
 
-test('send v2', (t) => {
+test('send v2', () => {
   // TODO: wrte a test that fails due to the bug in
   // MailSender.sendV2
-  t.end()
 })
