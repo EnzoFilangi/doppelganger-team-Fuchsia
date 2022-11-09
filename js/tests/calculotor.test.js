cf53a839b006c438cd94123c6a072b1caa59f537
Damien <damien.huang.paris@gmail.com>
2022-11-09 09:33:11 +0100

    calculator step1

--- js/tests/calculotor.test.js
+++ js/tests/calculotor.test.js
@@ -1,7 +1,22 @@
 const test = require('tape')
+const Calculator = require('../calculator')
 
 test('should throw when not authorized', (t) => {
   // TODO: write a test that fails due to the bug in
-  // Calculator.divide()
+
+  const numerator = 15
+  const denominator = 5
+
+
+  const auth = {
+    authorize:
+      () => true
+  }
+  const test = new Calculator(auth)
+
+  const caller = () => test.divide(numerator, denominator)
+  t.doesNotThrow(caller)
+
+
   t.end()
 })
