86553ccbce531e55f9480745615b2bd367d26117
Priestley DECELIAN <priestley.decelian@efrei.net>
2022-11-09 09:49:13 +0100

    add apply v1 test

--- js/tests/discount-applier.test.js
+++ js/tests/discount-applier.test.js
@@ -1,38 +1,53 @@
-const test = require('tape')
-const DiscountApplier = require('../discount-applier');
+const test = require("tape");
+const DiscountApplier = require("../discount-applier");
 
 class Notifier {
-  constructor () {
-  }
+  constructor() {}
 
   result = [];
+  count = 0;
 
-  notify (user,message) {
-    this.result.push(user + ' ' + message);
+  notify(user, message) {
+    this.result.push(user + " " + message);
+    this.count++;
   }
 }
 
-test('apply v1', (t) => {
+test("apply v1", (t) => {
   // TODO: write a test that fails due to the bug in
   // DiscountApplier.applyV1
-  t.end()
-})
 
-test('apply v2', (t) => {
+  let notifier = new Notifier();
+  let applier = new DiscountApplier(notifier);
+  const discount = 10;
+  const users = ["user1", "user2"];
+  const goodCallNumber = users.length;
 
+  applier.applyV1(discount, users);
+
+  t.equals(
+    notifier.count,
+    goodCallNumber,
+    `Notifier should be called ${users.length} times but was called ${notifier.count} times`
+  );
+
+  t.end();
+});
+
+test("apply v2", (t) => {
   let notifier = new Notifier();
 
   let applier = new DiscountApplier(notifier);
 
   let discount = 10;
-  let users = ['user1','user2','user3','user4'];
+  let users = ["user1", "user2", "user3", "user4"];
 
-  applier.applyV2(discount,users);
+  applier.applyV2(discount, users);
 
-  t.equal(notifier.result[0],`user1 You've got a new discount of 10`);
-  t.equal(notifier.result[1],`user2 You've got a new discount of 10`);
-  t.equal(notifier.result[2],`user3 You've got a new discount of 10`);
-  t.equal(notifier.result[3],`user4 You've got a new discount of 10`);
+  t.equal(notifier.result[0], `user1 You've got a new discount of 10`);
+  t.equal(notifier.result[1], `user2 You've got a new discount of 10`);
+  t.equal(notifier.result[2], `user3 You've got a new discount of 10`);
+  t.equal(notifier.result[3], `user4 You've got a new discount of 10`);
 
-  t.end()
-})
+  t.end();
+});
